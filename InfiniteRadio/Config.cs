306e6fedb438e0cc9ebd9e342c28d3586bcfd51a
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2021-12-28 13:02:56 +0500

    Add files via upload

---
+++ InfiniteRadio/Config.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Qurre.API.Addons;
+
+namespace InfiniteRadio
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "InfiniteRadio";
+
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+    }
+}
---
+++ InfiniteRadio/InfiniteRadio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Qurre;
+using Qurre.API.Events;
+
+namespace InfiniteRadio
+{
+    public class InfiniteRadio : Plugin
+    {
+        public override string Developer => "KoToXleB#4663";
+        public override string Name => "InfiniteRadio";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public Config CustomConfig { get; private set; }
+
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Player.RadioUsing += OnRadioUsing;
+        }
+
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Player.RadioUsing -= OnRadioUsing;
+        }
+
+        public static void OnRadioUsing(RadioUsingEvent ev)
+        {
+            ev.Battery = 100;
+        }
+    }
+}
